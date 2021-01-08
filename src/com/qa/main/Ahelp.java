34300c9e6ff79d2d6a8fe1feadbe81faa0465af7
Pjqa <>
2021-01-08 15:41:38 +0000

    added files

---
+++ src/com/qa/main/Ahelp.java
@@ -0,0 +1,30 @@
+package com.qa.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Ahelp {
+
+	List<String> numList = new ArrayList<>();
+	
+	  int[] numArray = {10, 23, 99, 14, 4, 7, 9, 69, 88, 53};{
+		  for(int a = 0; a <= numArray.length; a++); 
+	   
+	
+	List<Integer> numbArray = new ArrayList<Integer>();{
+	   for (int i = 0; i <= 10; i++) {
+		   numbArray.add(i);
+		   		System.out.println(i);
+		   
+	   for (int j = 1; j <= 1; j++) {
+				 System.out.println(i*10);
+	   	
+	
+	   		}
+	   }
+	  
+	}
+
+ }
+}
---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,31 @@
+package com.qa.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Ahelp ah = new Ahelp();
+		
+		ah.numList.add("10");
+		ah.numList.add("23");
+		ah.numList.add("99");
+		ah.numList.add("14");
+		ah.numList.add("4");
+		ah.numList.add("7");
+		ah.numList.add("9");
+		ah.numList.add("69");
+		ah.numList.add("88");
+		ah.numList.add("53");
+		
+		System.out.println(ah.numList);
+		
+		System.out.println(ah.numArray[3]);
+		
+
+		
+		//20DECSDET2
+      
+                   
+	}
+	
+}
